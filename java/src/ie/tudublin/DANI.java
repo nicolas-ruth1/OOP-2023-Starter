e164af671676673febdeb9d913def3518599351b
nicolas-ruth1 <c21388736@mytudublin.ie>
2023-04-26 18:19:58 +0100

    lastcommit

--- java/src/ie/tudublin/DANI.java
+++ java/src/ie/tudublin/DANI.java
@@ -12,6 +12,16 @@ public class DANI extends PApplet {
 		size(1000, 1000);
 	}
 
+	// method that is called once when the sketch starts, used to initialize the color mode and call the initializeModel() method to load the ".txt" file and build the word model
+	public void setup() {
+		colorMode(HSB);
+		initializeModel("shakespere.txt");
+		sonnet = writeSonnet();
+		for (String line : sonnet) {
+			println(line);
+		}
+	}
+
 	// method to display the sonnet on the screen
 	public void draw() {
 		background(0);
@@ -27,72 +37,12 @@ public class DANI extends PApplet {
 			text(sonnet[i], width / 2, yOffset + (i * lineHeight));
 		}
 	}
-	
-	// makes a new sonnet using the model
-	public String[] writeSonnet() {
-		String[] sonnet = new String[14];
-		for (int i = 0; i < 14; i++) {
-			sonnet[i] = writeLine();
-		}
-		return sonnet;
-	}
 
-	// method used to print the contents of the model ArrayList of Word objects
-	public void printModel() {
-		for (Word wordObj : model) {
-			println(wordObj.toString());
-		}
-	}
-
-	// makes a single line of the sonnet using the model
-	public String writeLine() {
-		StringBuilder sb = new StringBuilder();
-		int wordCount = 0;
-		Word currentWord = model.get((int) random(model.size()));
-
-		while (currentWord != null && wordCount < 8) {
-			sb.append(currentWord.getWord()).append(" ");
-			currentWord = pickRandomFollow(currentWord);
-			wordCount++;
-		}
-
-		return sb.toString().trim();
-	}
-
-	// method for writeLine that chooses random next word for given word
-	public Word pickRandomFollow(Word word) {
-		if (word.getFollows().size() == 0) {
-			return null;
-		}
-
-		String randomFollowWord = word.getFollows().get((int) random(word.getFollows().size())).getWord();
-		return findWord(randomFollowWord);
-	}
-	
 	// reads in a text file and creates a model of word frequencies and relationships
 	public void initializeModel(String filename) {
 		loadFile(filename);
 	}
 
-	// method that is called once when the sketch starts, used to initialize the color mode and call the initializeModel() method to load the ".txt" file and build the word model
-	public void setup() {
-		colorMode(HSB);
-		initializeModel("shakespere.txt");
-		sonnet = writeSonnet();
-		for (String line : sonnet) {
-			println(line);
-		}
-	}
-
-	// method called whenever a key is pressed making a new sonnet when the spacebar is pressed
-	public void keyPressed() {
-		if (key == ' ') {
-			sonnet = writeSonnet();
-		}
-	}	
-
-	float off = 0;
-	
 	// method for initializeModel that reads in the text file and updates the model
 	public void loadFile(String filename) {
 		String[] lines = loadStrings(filename);
@@ -123,6 +73,47 @@ public class DANI extends PApplet {
 		}
 	}
 
+	// makes a single line of the sonnet using the model
+	public String writeLine() {
+		StringBuilder sb = new StringBuilder();
+		int wordCount = 0;
+		Word currentWord = model.get((int) random(model.size()));
+
+		while (currentWord != null && wordCount < 8) {
+			sb.append(currentWord.getWord()).append(" ");
+			currentWord = pickRandomFollow(currentWord);
+			wordCount++;
+		}
+
+		return sb.toString().trim();
+	}
+	
+	// makes a new sonnet using the model
+	public String[] writeSonnet() {
+		String[] sonnet = new String[14];
+		for (int i = 0; i < 14; i++) {
+			sonnet[i] = writeLine();
+		}
+		return sonnet;
+	}
+
+	// method used to print the contents of the model ArrayList of Word objects
+	public void printModel() {
+		for (Word wordObj : model) {
+			println(wordObj.toString());
+		}
+	}
+
+	// method for writeLine that chooses random next word for given word
+	public Word pickRandomFollow(Word word) {
+		if (word.getFollows().size() == 0) {
+			return null;
+		}
+
+		String randomFollowWord = word.getFollows().get((int) random(word.getFollows().size())).getWord();
+		return findWord(randomFollowWord);
+	}
+
 	//  method that searches the model for a specific word and returns Word object
 	public Word findWord(String str) {
 		for (Word wordObj : model) {
@@ -133,4 +124,13 @@ public class DANI extends PApplet {
 		return null;
 	}
 
+	// method called whenever a key is pressed making a new sonnet when the spacebar is pressed
+	public void keyPressed() {
+		if (key == ' ') {
+			sonnet = writeSonnet();
+		}
+	}	
+
+	float off = 0;
+	
 }
